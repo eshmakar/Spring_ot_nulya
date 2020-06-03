95833ea65b2ae367fcbac0c5af2091684e130c27
Marat <eshmakar@mail.ru>
2020-06-03 12:57:52 +0300

    Initial commit

---
+++ src/main/java/papka/AppSpring.java
@@ -0,0 +1,12 @@
+package papka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSpring {
+    public static void main(String[] args) {
+        SpringApplication.run(AppSpring.class); //запуск приложения как spring
+
+    }
+}
